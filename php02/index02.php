a5661a167ebc2c0bc78543a2012cedf2b8d93012
Robertas <rcasnovicius@gmail.com>
2018-11-26 23:54:05 +0200

    php02

---
+++ php02/index02.php
@@ -0,0 +1,14 @@
+<?php $country = "Lietuva"; ?>
+<?php $city = "Vilnius"; ?>
+<?php $population = 574221; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?php echo $city .'-Lietuvos sostine'; ?></p>
+    <p><?php echo "$city ir $country sparciai auga"; ?></p>
+  </body>
+</html>
