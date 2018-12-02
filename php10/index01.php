eaaaf3e0935d44205f27a8323a1787962d795e67
Robertas <rcasnovicius@gmail.com>
2018-12-02 16:28:40 +0200

    php10

---
+++ php10/index01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    function plotas($a, $b) {
+      $suma = $a * $b;
+      echo "Staciakampio plotas: $suma";
+    }
+    plotas(10, 20);
+    ?>
+  </body>
+</html>
