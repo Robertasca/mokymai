5f141a877db0f08c68c701fdd95e0495d49e9281
Robertas <rcasnovicius@gmail.com>
2018-12-03 18:25:56 +0200

    php14

---
+++ php14/index02.php
@@ -0,0 +1,9 @@
+
+&copy; <?php echo date("Y"); ?>
+&copy; 2015-<?php echo date("Y"); ?>
+<?php function auto_copyright($year = 'auto'){ ?>
+   <?php if(intval($year) == 'auto'){ $year = date('Y'); } ?>
+   <?php if(intval($year) == date('Y')){ echo intval($year); } ?>
+   <?php if(intval($year) < date('Y')){ echo intval($year) . ' - ' . date('Y'); } ?>
+   <?php if(intval($year) > date('Y')){ echo date('Y'); } ?>
+<?php } ?>
